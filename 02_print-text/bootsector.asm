8045385df961fe34b61017ead49fc80a5952a3a9
normalgamer <36603719+normalgamer@users.noreply.github.com>
2019-03-17 11:51:03 +0100

    Create bootsector.asm

---
+++ 02_print-text/bootsector.asm
@@ -0,0 +1,10 @@
+```
+mov ah, 0x0E          ; Teletype mode
+mov al, 'X'           ; Store in al the char we want to print
+int 0x10              ; BIOS video interrupt
+
+jmp $                 ; Infinite loop
+
+times 510-($-$$) db 0 ; Fill the rest of the sector with zeroes
+dw 0xAA55             ; Boot signature
+```
