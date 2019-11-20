f084174d2fb270ff2c64c99072582020420b63b6
normalgamer <36603719+normalgamer@users.noreply.github.com>
2019-11-20 20:20:19 +0100

    Update bootsector.asm

--- 02_print-text/bootsector.asm
+++ 02_print-text/bootsector.asm
@@ -1,4 +1,3 @@
-```
 mov ah, 0x0E          ; Teletype mode
 mov al, 'X'           ; Store in al the char we want to print
 int 0x10              ; BIOS video interrupt
@@ -7,4 +6,3 @@ jmp $                 ; Infinite loop
 
 times 510-($-$$) db 0 ; Fill the rest of the sector with zeroes
 dw 0xAA55             ; Boot signature
-```
